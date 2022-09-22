c477bbf75441c51cc78a25c96c688b92f0706977
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-09-22 16:28:00 +0700

    done logger

--- src/main/java/cg/wbd/grandemonstration/AppConfig.java
+++ src/main/java/cg/wbd/grandemonstration/AppConfig.java
@@ -98,7 +98,7 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/cms");
         dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+        dataSource.setPassword("88888888");
         return dataSource;
     }
 
--- src/main/java/cg/wbd/grandemonstration/logger/Logger.java
+++ src/main/java/cg/wbd/grandemonstration/logger/Logger.java
@@ -1,17 +1,28 @@
 package cg.wbd.grandemonstration.logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Aspect
+// Aspect i
 @Component
 public class Logger {
-    public void error() {
-        System.out.println("[CMS] ERROR!");
-    }
-
-    @AfterThrowing(pointcut = "execution(public * cg.wbd.grandemonstration.service.CustomerService.findAll(..))")
-    public void log() {
+    @AfterThrowing(pointcut = "execution(public * cg.wbd.grandemonstration.service.CustomerService.*(..))", throwing = "e")
+    // pointcut là một biểu thức chứa các điều kiện để xác định các phương thức nào sẽ được chạy trước hoặc sau khi chạy phương thức mà chúng ta muốn theo dõi.
+    // execution(public * cg.wbd.grandemonstration.service.CustomerService.*(..))
+    // public: là quyền truy cập của phương thức
+    // *: là kiểu trả về của phương thức
+    // cg.wbd.grandemonstration.service.CustomerService: là tên của class chứa phương thức
+    // *: là tên của phương thức
+    // (..): là tham số của phương thức
+    public void log(JoinPoint joinPoint, Exception e) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        System.out.println(String.format("[CMS] co loi xay ra: %s.%s%s: %s", className, method, args, e.getMessage()));
     }
 }
